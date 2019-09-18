51da2044ce7ceeb5acf3741a24acc362843ba370
quedel <hwenzel@gmail.com>
2019-09-18 10:14:50 +0200

    added index2.php to query multiple unifi controllers

---
+++ public/index2.php
@@ -0,0 +1,72 @@
+<?php
+
+use ISPServerfarm\UnifiMeshviewer\MeshviewerGenerator;
+use Dotenv\Dotenv;
+
+// load the class using the composer autoloader
+require_once('../vendor/autoload.php');
+
+// load Whoops
+if (class_exists("\Whoops\Run")){
+    $whoops = new \Whoops\Run;
+    $whoops->prependHandler(new \Whoops\Handler\PrettyPageHandler);
+    $whoops->register();
+}
+
+// set Content Type to application/json
+header('Content-Type: application/json');
+
+// set TimeZone
+date_default_timezone_set(getenv('TIMEZONE'));
+
+if(file_exists("../unifi.json")){
+    $unifi =  json_decode(file_get_contents("../unifi.json"),true);
+
+    $ret_status = array();
+
+    foreach ($unifi['controller'] as $env) {
+
+        if(file_exists('.env.'.$env)) {
+
+            // load dotenv and the .env file.
+            $dotenv = Dotenv::create(dirname(dirname(__FILE__)), '.env.'.$env);
+            $dotenv->load(true);
+
+
+            // Initiate the MeshviewerGenerator
+            $meshGenerator = new MeshviewerGenerator();
+
+            $ret_status[$env] = json_decode($meshGenerator->executeTask());
+
+            if(file_exists("data/nodelist.json")) {
+                
+                if(!is_dir("data/$env/")) mkdir("data/$env/", 0700);
+                if(file_exists("data/nodes.json")) copy("data/nodes.json", "data/$env/nodes.json");
+                if(file_exists("data/graph.json")) copy("data/graph.json", "data/$env/graph.json");
+                if(file_exists("data/nodelist.json")) copy("data/nodelist.json", "data/$env/nodelist.json");
+                if(file_exists("data/meshviewer.json")) copy("data/meshviewer.json", "data/$env/meshviewer.json");
+
+            }
+            
+        }
+    }
+
+    echo json_encode($ret_status,JSON_PRETTY_PRINT);
+
+}
+else {
+
+    // load dotenv and the .env file.
+    $dotenv = Dotenv::create(dirname(dirname(__FILE__)));
+    $dotenv->load(true);
+
+
+    // Initiate the MeshviewerGenerator
+    $meshGenerator = new MeshviewerGenerator();
+
+    // Generate the JSON Files
+    $status = $meshGenerator->executeTask();
+
+    echo $status;
+    }
+?>
