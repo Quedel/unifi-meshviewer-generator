501f55f3b7f4d7a129375d4c796d555483e7282b
quedel <hwenzel@gmail.com>
2020-06-30 13:14:59 +0200

    change index2.php to index.php

--- public/index.php
+++ public/index.php
@@ -13,21 +13,61 @@ if (class_exists("\Whoops\Run")){
     $whoops->register();
 }
 
-// load dotenv and the .env file.
-$dotenv = Dotenv::create(dirname(dirname(__FILE__)));
-$dotenv->load(true);
-
-// set TimeZone
-date_default_timezone_set(getenv('TIMEZONE'));
-
 // set Content Type to application/json
 header('Content-Type: application/json');
 
-// Initiate the MeshviewerGenerator
-$meshGenerator = new MeshviewerGenerator();
+if(file_exists("../unifi.json")){
+    $unifi =  json_decode(file_get_contents("../unifi.json"),true);
 
-// Generate the JSON Files
-$status = $meshGenerator->executeTask();
+    $ret_status = array();
 
-echo $status;
+    foreach ($unifi['controller'] as $env) {
+
+        if(file_exists('../.env.'.$env)) {
+
+            // load dotenv and the .env file.
+            $dotenv = Dotenv::create(dirname(dirname(__FILE__)), '.env.'.$env);
+            $dotenv->overload(true);
+
+            // set TimeZone
+            date_default_timezone_set(getenv('TIMEZONE'));
+
+            // Initiate the MeshviewerGenerator
+            $meshGenerator = new MeshviewerGenerator();
+
+            $ret_status[$env] = json_decode($meshGenerator->executeTask());
+
+            if(file_exists("data/nodelist.json")) {
+                
+                if(!is_dir("data/$env/")) mkdir("data/$env/", 0700);
+                if(file_exists("data/nodes.json")) rename("data/nodes.json", "data/$env/nodes.json");
+                if(file_exists("data/graph.json")) rename("data/graph.json", "data/$env/graph.json");
+                if(file_exists("data/nodelist.json")) rename("data/nodelist.json", "data/$env/nodelist.json");
+                if(file_exists("data/meshviewer.json")) rename("data/meshviewer.json", "data/$env/meshviewer.json");
+
+            }
+            
+        }
+    }
+
+    echo json_encode($ret_status,JSON_PRETTY_PRINT);
+
+}
+else {
+
+    // load dotenv and the .env file.
+    $dotenv = Dotenv::create(dirname(dirname(__FILE__)));
+    $dotenv->load(true);
+
+    // set TimeZone
+    date_default_timezone_set(getenv('TIMEZONE'));    
+
+    // Initiate the MeshviewerGenerator
+    $meshGenerator = new MeshviewerGenerator();
+
+    // Generate the JSON Files
+    $status = $meshGenerator->executeTask();
+
+    echo $status;
+    }
 ?>
